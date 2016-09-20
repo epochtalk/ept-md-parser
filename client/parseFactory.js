97405abf590faec3ff7551ce63cd5eab4a8102a0
Edward Kim <ed@slickage.com>
2016-09-20 12:29:26 -1000

    1.0.3: Preserve whitespace
    CommonMark, by default, will greedily eat any newlines between
    paragraphs, this doesn't work well with the BBCode parser or how
    epochtalk displays whitespace. Newlines chars are substituted out to
    preserve them while being run through the parser, then replaced
    afterward.

--- client/parseFactory.js
+++ client/parseFactory.js
@@ -3,7 +3,12 @@ var renderer;
 
 function parse(input) {
   if (!input) { input = ''; }
+  // preserve whitespacing
+  input = input.replace(/\r\n|\r|\n/g, String.fromCharCode(26));
+  // compile markdown
   input = compiler(input, { renderer: renderer });
+  // replace whitespacing
+  input = input.replace(new RegExp(String.fromCharCode(26), 'g'), '\n');
   return input;
 }
 
